19583521a06029d158387f1be59abc847451c0e8
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-18 13:18:25 +1000

    4 working ordertabs e2e tests

--- test/e2e/order.spec.js
+++ test/e2e/order.spec.js
@@ -1,45 +1,41 @@
 describe('WIP e2e testing', function () {
 
-    describe(' accounts list', function () {
+    describe('orders tab', function () {
         var accountsList;
 
         beforeEach(function () {
             browser.get('http://localhost:9000/');
         });
 
-        it('should filter by accountname nr results is 2', function () {
-            element(by.model('accountName')).sendKeys('ha');
-            accountsList = element.all(by.repeater('account in $parent.filtered'));
-            //expect(accountsList.count()).toEqual(2);
-            expect(1).toEqual(2);
+        it('should filter by accountname nr results is 1', function () {
+            element(by.model('accountName')).sendKeys('je');
+            accountsList = element.all(by.repeater('account in accounts'));
+            expect(accountsList.count()).toEqual(1);
         });
 
-        //it('should filter by adviser nr results is 1', function () {
-        //    element(by.model('accountName')).sendKeys('bbb');
-        //    accountsList = element.all(by.repeater('account in $parent.filtered'));
-        //    expect(accountsList.count()).toEqual(1);
-        //});
-        //
-        //it('should return 0', function () {
-        //    element(by.model('accountName')).sendKeys('mxcnvmnxmvnxmnvmn');
-        //    accountsList = element.all(by.repeater('account in $parent.filtered'));
-        //    expect(accountsList.count()).toEqual(0);
-        //});
-        //
-        //it('should resetfilter', function () {
-        //    element(by.model('accountName')).sendKeys('bbb');
-        //    element(by.css('body > div > div > div.resetFilter > a:nth-child(1)')).click();
-        //
-        //    accountsList = element.all(by.repeater('account in $parent.filtered'));
-        //    expect(accountsList.count()).toEqual(10);
-        //});
-        //
+        it('should filter by adviser nr results is 1', function () {
+            element(by.model('accountName')).sendKeys('bbb');
+            accountsList = element.all(by.repeater('account in accounts'));
+            expect(accountsList.count()).toEqual(2);
+        });
+
+        it('should return 0', function () {
+            element(by.model('accountName')).sendKeys('mxcnvmnxmvnxmnvmn');
+            accountsList = element.all(by.repeater('account in accounts'));
+            expect(accountsList.count()).toEqual(0);
+        });
+
+        it('should resetfilter', function () {
+            element(by.model('accountName')).sendKeys('bbb');
+            element(by.css('body > div > div.ng-scope > div > div.resetFilter > a:nth-child(1)')).click();
+            accountsList = element.all(by.repeater('account in accounts'));
+            expect(accountsList.count()).toEqual(10);
+        });
+
         //it('should return same amount results', function () {
         //    element(by.css('body > div > div > div.orders.ng-scope > div > div > div > div > div > div > div > div > div > div > ul > li:nth-child(6) > div > button.btn.btn-default.active')).click();
-        //
-        //    accountsList = element.all(by.repeater('account in $parent.filtered'));
-        //
-        //    expect(accountsList.count()).toEqual(27);
+        //    accountsList = element.all(by.repeater('account in accounts'));
+        //    expect(accountsList.count()).toEqual(10);
         //});
 
 
