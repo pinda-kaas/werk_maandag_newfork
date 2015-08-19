0a8edc9c2e0a0236bfa99737e90a0cfd2ceef8bc
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-19 13:43:51 +1000

    fixed columwidths with stratio directivve

---
+++ app/modules/common/orders/directives/stRatio.js
@@ -0,0 +1,10 @@
+app.directive('stRatio',function(){
+    return {
+        link:function(scope, element, attr){
+            var ratio=+(attr.stRatio);
+
+            element.css('width',ratio+'%');
+
+        }
+    };
+});
