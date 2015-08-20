538ded7da5204d830b68b3d7badc20f257e8bb9b
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-20 17:06:01 +1000

    orderdetail 1st version mockdata

--- app/modules/common/orders/controllers/orderDetailCtrl.js
+++ app/modules/common/orders/controllers/orderDetailCtrl.js
@@ -1,6 +1,8 @@
 app.controller('OrderDetailCtrl', function ($scope, $filter, $location,configService, orderDetail) {
 
   $scope.orderDetail=  orderDetail;
+
+  $scope.detailRows= orderDetail.rows;
   console.log('orderdetail ctrl',orderDetail);
 
 });
