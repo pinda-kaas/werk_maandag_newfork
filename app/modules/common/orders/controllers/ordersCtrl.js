285e92211ce888c01690c6a19780cea74805b43e
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-24 16:30:10 +1000

    first version for advisers filter

--- app/modules/common/orders/controllers/ordersCtrl.js
+++ app/modules/common/orders/controllers/ordersCtrl.js
@@ -1,10 +1,14 @@
-app.controller('OrdersCtrl', function ($scope, $filter, $location, configService, orderData,settlementsData,completeOrders) {
+app.controller('OrdersCtrl', function ($scope, $filter, $location, configService, orderData, settlementsData, completeOrders) {
 
-    console.log('ordersctrl started');
+  console.log('ordersctrl started');
 
-    $scope.accounts = orderData;
+  $scope.accounts = orderData;
 
-    $scope.settlements=settlementsData;
+  $scope.settlements = settlementsData;
 
-    $scope.completeOrders=completeOrders;
+  $scope.completeOrders = completeOrders;
+
+  $scope.selectAdviser = function (adviser) {
+    $scope.adviser= adviser;
+  }
 });
