5d9c995a5f4bc52519f83a98634d6f8a649d5b4c
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-26 13:02:54 +1000

    grid style updated orders tab

--- app/modules/common/orders/directives/stRatio.js
+++ app/modules/common/orders/directives/stRatio.js
@@ -1,6 +1,7 @@
 app.directive('stRatio',function(){
     return {
         link:function(scope, element, attr){
+          console.log('stratio');
             var ratio=+(attr.stRatio);
             element.css('width',ratio+'%');
         }
